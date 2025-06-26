b5e03c8dc2c3d5e23facb208bd89c640c9e242e6
André Brito Vita <andrebritovita.dev@hotmail.com>
2025-06-26 12:05:45 -0300

    Criação da pasta asset Colocou-se os arquivos na pasta assets Criação do Menu: Background, sons e fontes. Criação do arquivo de constantes

--- code/Game.py
+++ code/Game.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import pygame as pg
+import pygame
+import pygame.mixer
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
-        pg.init()
-        self.window = pg.display.set_mode(size=(600, 480))
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-            # Check for all events
-            # for event in pg.event.get():
-            #     if event.type == pg.QUIT:
-            #         pg.quit()  # Close Window
-            #         quit()  # End pygame
