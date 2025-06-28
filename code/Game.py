d2d06e46fac3c1543446f2ab74bc23b6082534d2
André Brito Vita <andrebritovita.dev@hotmail.com>
2025-06-28 09:41:58 -0300

    Implementação de textos no Level 1 e função de quit

--- code/Game.py
+++ code/Game.py
@@ -23,7 +23,7 @@ class Game:
             # Se menu_option for qualquer uma das Strings das posições 0, 1 ou 2
             # a condição será True e o bloco de código será executado
             if menu_return in ([MENU_OPTION[0], MENU_OPTION[1],MENU_OPTION[2]]):
-                level = Level(self.window, 'Level 1', menu_return)
+                level = Level(self.window, 'Level1', menu_return)
                 level_return = level.run()
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()  # Close Window
