28c666073c36a55733934ee401fd2d1ddd28fbdd
André Brito Vita <andrebritovita.dev@hotmail.com>
2025-06-30 11:44:28 -0300

    Implementação do SQLite3 para o Score usando o Design Patter Proxy

--- code/Game.py
+++ code/Game.py
@@ -7,6 +7,7 @@ from pygame import event
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.Level import Level
 from code.Menu import Menu
+from code.Score import Score
 
 
 class Game:
@@ -17,6 +18,7 @@ class Game:
     def run(self):
 
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()  # Recebe uma String
 
@@ -29,6 +31,11 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, plyer_score)
                     level_return = level.run(plyer_score)
+                    if level_return:
+                        score.save(menu_return, plyer_score)
+
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
 
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()  # Close Window
