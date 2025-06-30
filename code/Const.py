013d499835ba91dbc7ff1c81ffe8b93bee0cee0a
André Brito Vita <andrebritovita.dev@hotmail.com>
2025-06-30 10:04:40 -0300

    Implementação do Level2

--- code/Const.py
+++ code/Const.py
@@ -129,7 +129,7 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL}
 
 # S
-SPAWN_TIME = 4000
+SPAWN_TIME = 4000 # Tempo para novos inimigos aparecerem na tela
 
 # T
 TIMEOUT_STEP = 100  # 100ms
--- code/Game.py
+++ code/Game.py
@@ -22,9 +22,14 @@ class Game:
 
             # Se menu_option for qualquer uma das Strings das posições 0, 1 ou 2
             # a condição será True e o bloco de código será executado
-            if menu_return in ([MENU_OPTION[0], MENU_OPTION[1],MENU_OPTION[2]]):
-                level = Level(self.window, 'Level1', menu_return)
-                level_return = level.run()
+            if menu_return in ([MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]):
+                plyer_score = [0, 0]  # [Player1, Player1]
+                level = Level(self.window, 'Level1', menu_return, plyer_score)
+                level_return = level.run(plyer_score)
+                if level_return:
+                    level = Level(self.window, 'Level2', menu_return, plyer_score)
+                    level_return = level.run(plyer_score)
+
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()  # Close Window
                 quit()  # End pygame
